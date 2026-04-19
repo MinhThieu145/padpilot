4d50951ace535156c8a2429face3866a2ae4c0fa
MinhThieu145 <nguyen864@usf.edu>
2026-04-19 17:33:00 +0700

    feat: create base classes for the 3 ResponseMode

---
+++ ChatVisual/ResponseModeSettings.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ChatVisual
+{
+    internal class ResponseModeSettings
+    {
+        public string SystemPrompt { get; set; }
+        public AIProviderConfig Primary { get; set; }
+        public AIProviderConfig Fallback { get; set; }
+    }
+}
---
+++ ChatVisual/ResponseModeSettingsSet.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ChatVisual
+{
+    internal class ResponseModeSettingsSet
+    {
+        public ResponseModeSettings Quick { get; set; }
+        public ResponseModeSettings Thinking { get; set; }
+        public ResponseModeSettings DeepThinking { get; set; }
+
+    }
+}
